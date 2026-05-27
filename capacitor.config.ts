82d2dd86e651babd7b86be037b0dbb49b3745062
Marina Carvalho <masocacads@gmail.com>
2026-05-26 22:11:19 -0300

    Adicionar configuração Ionic+Angular

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.mobileticketsIonic',
+  appName: 'MobileTicketsIonic',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
